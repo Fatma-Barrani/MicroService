267ee1fec41c46e34bd24bb5faf321c72afcae57
Fatma-Barrani <fatma.BARRANI@esprit.tn>
2026-04-25 11:32:03 +0000

    enseignant crud + eureka

--- src/main/java/tn/esprit/spring/microserviceproject/Dtos/enseignantRequestDto.java
+++ src/main/java/tn/esprit/spring/microserviceproject/Dtos/enseignantRequestDto.java
@@ -1,4 +1,15 @@
 package tn.esprit.spring.microserviceproject.Dtos;
 
+import lombok.*;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class enseignantRequestDto {
+    private String nom;
+    private String prenom;
+    private String email;
+    private String telephone;
+    private String specialite;
 }
